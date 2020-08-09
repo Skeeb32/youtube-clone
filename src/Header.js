a42e530a67dfa561b3aabc84b118a0d0e3277930
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-08-09 01:12:28 -0700

    FlexBox

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Header.css';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
@@ -6,26 +7,30 @@ import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import Avatar from '@material-ui/core/Avatar';
 
-
 function Header() {
   return (
     <div class Name="header">
-      <h1>I am a header</h1>
-      <MenuIcon />
-      <img
-        className="header__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1024px-Logo_of_YouTube_%282015-2017%29.svg.png"
-        alt=""
-      />
-      <input type="text" />
-      <SearchIcon />
-      <VideoCallIcon />
-      <AppsIcon />
-      <NotificationsIcon />
-      <Avatar 
-      alt="Shaqib Habib"
-      src="https://avatars0.githubusercontent.com/u/55864051?s=400&u=d17ca8209ac548d3d3033ddbe93f9741b7123263&v=4"
-      />
+      <div className="header__left">
+        <MenuIcon />
+        <img
+          className="header__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1024px-Logo_of_YouTube_%282015-2017%29.svg.png"
+          alt=""
+        />
+      </div>
+      <div className="header__input">
+        <input type="text" />
+      </div>
+      <div className="header__icons">
+        <SearchIcon />
+        <VideoCallIcon />
+        <AppsIcon />
+        <NotificationsIcon />
+        <Avatar
+          alt="Shaqib Habib"
+          src="https://avatars0.githubusercontent.com/u/55864051?s=400&u=d17ca8209ac548d3d3033ddbe93f9741b7123263&v=4"
+        />
+      </div>
     </div>
   );
 }
