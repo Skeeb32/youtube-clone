d343a4e0d0131857ae57f72a65c39eb20e3ce3d4
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-08-10 01:03:30 -0700

    Recommended Videos Done

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,12 +1,112 @@
-import React from 'react'
-import "./RecommendedVideos.css"
+import React from 'react';
+import './RecommendedVideos.css';
+import VideoCard from './VideoCard';
 
 function RecommendedVideos() {
-    return (
-        <div className="recommendedVideos">
-            <h2>Creating a Youtube Clone...What do yall think?</h2>
-        </div>
-    )
+  return (
+    <div className="recommendedVideos">
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2020"
+          views="3.2M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.ytimg.com/an_webp/tCAt8eEKPDc/mqdefault_6s_480x270.webp?du=3000&sqp=CObdw_kF&rs=AOn4CLDLnF5TJmv7JmhXgvjPZywgPKgfdQ"
+          channel="Shaqib Habib"
+          image="https://i.ytimg.com/vi/J6rVaFzOEP8/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC50RZg-h1YCrv-n8T8kNoUJH6BAg"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2020"
+          views="3.2M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.ytimg.com/an_webp/tCAt8eEKPDc/mqdefault_6s_480x270.webp?du=3000&sqp=CObdw_kF&rs=AOn4CLDLnF5TJmv7JmhXgvjPZywgPKgfdQ"
+          channel="Shaqib Habib"
+          image="https://i.ytimg.com/vi/J6rVaFzOEP8/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC50RZg-h1YCrv-n8T8kNoUJH6BAg"
+        />
+
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2020"
+          views="3.2M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.ytimg.com/an_webp/tCAt8eEKPDc/mqdefault_6s_480x270.webp?du=3000&sqp=CObdw_kF&rs=AOn4CLDLnF5TJmv7JmhXgvjPZywgPKgfdQ"
+          channel="Shaqib Habib"
+          image="https://i.ytimg.com/vi/J6rVaFzOEP8/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC50RZg-h1YCrv-n8T8kNoUJH6BAg"
+        />
+
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2020"
+          views="3.2M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.ytimg.com/an_webp/tCAt8eEKPDc/mqdefault_6s_480x270.webp?du=3000&sqp=CObdw_kF&rs=AOn4CLDLnF5TJmv7JmhXgvjPZywgPKgfdQ"
+          channel="Shaqib Habib"
+          image="https://i.ytimg.com/vi/J6rVaFzOEP8/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC50RZg-h1YCrv-n8T8kNoUJH6BAg"
+        />
+
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2020"
+          views="3.2M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.ytimg.com/an_webp/tCAt8eEKPDc/mqdefault_6s_480x270.webp?du=3000&sqp=CObdw_kF&rs=AOn4CLDLnF5TJmv7JmhXgvjPZywgPKgfdQ"
+          channel="Shaqib Habib"
+          image="https://i.ytimg.com/vi/J6rVaFzOEP8/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC50RZg-h1YCrv-n8T8kNoUJH6BAg"
+        />
+
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2020"
+          views="3.2M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.ytimg.com/an_webp/tCAt8eEKPDc/mqdefault_6s_480x270.webp?du=3000&sqp=CObdw_kF&rs=AOn4CLDLnF5TJmv7JmhXgvjPZywgPKgfdQ"
+          channel="Shaqib Habib"
+          image="https://i.ytimg.com/vi/J6rVaFzOEP8/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC50RZg-h1YCrv-n8T8kNoUJH6BAg"
+        />
+
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2020"
+          views="3.2M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.ytimg.com/an_webp/tCAt8eEKPDc/mqdefault_6s_480x270.webp?du=3000&sqp=CObdw_kF&rs=AOn4CLDLnF5TJmv7JmhXgvjPZywgPKgfdQ"
+          channel="Shaqib Habib"
+          image="https://i.ytimg.com/vi/J6rVaFzOEP8/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC50RZg-h1YCrv-n8T8kNoUJH6BAg"
+        />
+
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2020"
+          views="3.2M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.ytimg.com/an_webp/tCAt8eEKPDc/mqdefault_6s_480x270.webp?du=3000&sqp=CObdw_kF&rs=AOn4CLDLnF5TJmv7JmhXgvjPZywgPKgfdQ"
+          channel="Shaqib Habib"
+          image="https://i.ytimg.com/vi/J6rVaFzOEP8/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC50RZg-h1YCrv-n8T8kNoUJH6BAg"
+        />
+
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2020"
+          views="3.2M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.ytimg.com/an_webp/tCAt8eEKPDc/mqdefault_6s_480x270.webp?du=3000&sqp=CObdw_kF&rs=AOn4CLDLnF5TJmv7JmhXgvjPZywgPKgfdQ"
+          channel="Shaqib Habib"
+          image="https://i.ytimg.com/vi/J6rVaFzOEP8/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC50RZg-h1YCrv-n8T8kNoUJH6BAg"
+        />
+
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2020"
+          views="3.2M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.ytimg.com/an_webp/tCAt8eEKPDc/mqdefault_6s_480x270.webp?du=3000&sqp=CObdw_kF&rs=AOn4CLDLnF5TJmv7JmhXgvjPZywgPKgfdQ"
+          channel="Shaqib Habib"
+          image="https://i.ytimg.com/vi/J6rVaFzOEP8/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC50RZg-h1YCrv-n8T8kNoUJH6BAg"
+        />
+
+        <VideoCard
+          title="Become a Web Developer in 10 minutes | 2020"
+          views="3.2M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.ytimg.com/an_webp/tCAt8eEKPDc/mqdefault_6s_480x270.webp?du=3000&sqp=CObdw_kF&rs=AOn4CLDLnF5TJmv7JmhXgvjPZywgPKgfdQ"
+          channel="Shaqib Habib"
+          image="https://i.ytimg.com/vi/J6rVaFzOEP8/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC50RZg-h1YCrv-n8T8kNoUJH6BAg"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default RecommendedVideos
+export default RecommendedVideos;
