c78de9ad088cb83b2998f7d402179a1bb871e01c
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-08-10 03:12:44 -0700

    ChannelRow CSS

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ChannelRow from './ChannelRow';
 import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined';
 import './SearchPage.css';
 
@@ -10,6 +11,19 @@ function SearchPage() {
         <h2>Filter</h2>
       </div>
       <hr />
+      <ChannelRow 
+      image="https://i.ytimg.com/an_webp/ZHCWuNBZexs/mqdefault_6s.webp?du=3000&sqp=CJWTxPkF&rs=AOn4CLDfV34dL4bv4sn4x7ny8QsqpelMxA"
+      channel="Starter Squad"
+      verified
+      subs="732K"
+      noOfVideos={382}
+      description="The starter Pokemon rebel against humanity and decide to evolve themselves."
+      
+      />
+
+
+
+
     </div>
   );
 }
